af3eb78def47599a919c54d54e70fbad8af52dc6
apple <apple@Divines-MacBook-Pro-2.local>
2023-03-07 14:08:46 +0100

    prostgress added

--- complaint_management_system/staff/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import StaffModel
-
-# Register your models here.
-@admin.register(StaffModel)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('staff_number', 'department')
-    
--- complaint_management_system/staff/utils.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import EmailMessage
-
-
-class Util:
-    @staticmethod
-    def send_email(data):
-        
-        email = EmailMessage(subject=data['subject'], body=data['message'], to=data['to'])
-        email.send()
